eb18524b5cb3f17222d606550d35f5dd839ec71a
ayham291 <ayham.dev@ayhamcloud.de>
2024-05-23 10:45:57 +0200

    update server example

--- examples/server/server.go
+++ examples/server/server.go
@@ -1,135 +1,126 @@
-/* server.go
- *
- * Copyright (C) 2006-2022 wolfSSL Inc.
- *
- * This file is part of wolfSSL.
- *
- * wolfSSL is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * wolfSSL is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
- */
-
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
-    wolfSSL "github.com/wolfssl/go-wolfssl"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+
+	wolfSSL "github.com/ayham291/go-wolfssl"
 )
 
 /* Connection configuration constants */
 const (
-    CONN_HOST = "localhost"
-    CONN_PORT = "11111"
-    CONN_TYPE = "tcp"
+	CONN_HOST = "localhost"
+	CONN_PORT = "11111"
+	CONN_TYPE = "tcp"
 )
 
 func main() {
-    /* Server Key and Certificate paths */
-    CERT_FILE := "../certs/server-cert.pem"
-    KEY_FILE  := "../certs/server-key.pem"
+  fmt.Printf("%s\n", wolfSSL.WolfSSL_lib_version())
+	/* Server Key and Certificate paths */
+	CERT_FILE := "./certs/server_cert.pem"
+	KEY_FILE := "./certs/server_key.pem"
 
-    /* Initialize wolfSSL */
-    wolfSSL.WolfSSL_Init()
+	/* Initialize wolfSSL */
+	method := wolfSSL.Method{Name: "TLSv1.3"}
 
-    /* Create WOLFSSL_CTX with tlsv12 */
-    ctx := wolfSSL.WolfSSL_CTX_new(wolfSSL.WolfTLSv1_2_server_method())
-    if ctx == nil {
-        fmt.Println(" WolfSSL_CTX_new Failed");
-        os.Exit(1)
-    }
+  ctx := wolfSSL.InitWolfSSL(CERT_FILE, KEY_FILE, true, method)
 
-    /* Load server certificates into WOLFSSL_CTX */
-    ret := wolfSSL.WolfSSL_CTX_use_certificate_file(ctx, CERT_FILE, wolfSSL.SSL_FILETYPE_PEM)
-    if ret != wolfSSL.WOLFSSL_SUCCESS {
-        fmt.Println("Error: WolfSSL_CTX_use_certificate Failed");
-        os.Exit(1)
-    }
 
-    /* Load server key into WOLFSSL_CTX */
-    ret = wolfSSL.WolfSSL_CTX_use_PrivateKey_file(ctx, KEY_FILE, wolfSSL.SSL_FILETYPE_PEM)
-    if ret != wolfSSL.WOLFSSL_SUCCESS {
-        fmt.Println("Error: WolfSSL_CTX_use_PrivateKey Failed");
-        os.Exit(1)
-    }
+	/* Listen for incoming connections */
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	/* Close the listener when the application closes */
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
 
-    /* Listen for incoming connections */
-    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-    if err != nil {
-        fmt.Println("Error listening:", err.Error())
-        os.Exit(1)
-    }
-    /* Close the listener when the application closes */
-    defer l.Close()
-    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
 
-    /* Listen for an incoming connection */
-    conn, err := l.Accept()
-    if err != nil {
-        fmt.Println("Error accepting: ", err.Error())
-    }
+	/* Listen for an incoming connection */
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				fmt.Println("Error accepting: ", err.Error())
+				os.Exit(1)
+			}
+			/* Handle connections concurrently */
+			go handleRequest(conn, (*wolfSSL.WOLFSSL_CTX)(ctx))
+		}
+	}()
 
-    /* Create a WOLFSSL object */
-    ssl := wolfSSL.WolfSSL_new(ctx)
-    if ssl == nil {
-        fmt.Println(" WolfSSL_new Failed");
-        os.Exit(1)
-    }
+	/* Wait for a signal to shutdown */
+	got := <-sig
+	fmt.Println("Received signal:", got)
 
-    /* Retrieve file descriptor from net.Conn type */
-    file,err := conn.(*net.TCPConn).File()
-    fd := file.Fd()
-    wolfSSL.WolfSSL_set_fd(ssl, int(fd))
-
-    /* Establish TLS connection */
-    ret = wolfSSL.WolfSSL_accept(ssl);
-    if ret != wolfSSL.WOLFSSL_SUCCESS {
-        fmt.Println(" WolfSSL_accept error ", ret);
-        os.Exit(1)
-    } else {
-        fmt.Println("Client Succesfully Connected!");
-    }
-
-    buf := make([]byte, 256)
-
-    /* Recieve then print the message from client */
-    ret = wolfSSL.WolfSSL_read(ssl, buf, 256)
-    if ret == -1 {
-        fmt.Println(" WolfSSL_read failed ");
-    } else {
-        fmt.Println("Client says : ", string(buf));
-    }
-
-    /* Create the message and send to client */
-    reply := []byte("I hear ya fashizzle!")
-    sz := uintptr(len(reply))
-
-    ret = wolfSSL.WolfSSL_write(ssl, reply, sz)
-    if uintptr(ret) != sz {
-        fmt.Println(" WolfSSL_write failed ");
-        os.Exit(1)
-    }
-
-    /* Shutdown wolfSSL */
-    wolfSSL.WolfSSL_shutdown(ssl)
-    /* Free wolfSSL and wolfSSL_CTX objects */
-    wolfSSL.WolfSSL_free(ssl)
-    wolfSSL.WolfSSL_CTX_free(ctx)
-    /* Cleanup the wolfSSL environment */
-    wolfSSL.WolfSSL_Cleanup()
-
-    /* Close the connection */
-    conn.Close()
+	/* Shutdown wolfSSL */
+	/* Free wolfSSL and wolfSSL_CTX objects */
+	/* Cleanup wolfSSL_CTX object */
+	wolfSSL.WolfSSL_CTX_free((*wolfSSL.WOLFSSL_CTX)(ctx))
+	/* Cleanup the wolfSSL environment */
+	wolfSSL.WolfSSL_Cleanup()
 }
 
+/* Handles incoming requests */
+func handleRequest(conn net.Conn, ctx *wolfSSL.WOLFSSL_CTX) {
+	/* Create a WOLFSSL object */
+	ssl := wolfSSL.WolfSSL_new((*wolfSSL.WOLFSSL_CTX)(ctx))
+	if ssl == nil {
+		fmt.Println("WolfSSL_new Failed")
+		os.Exit(1)
+	}
+	var ret int
+	/* Retrieve file descriptor from net.Conn type */
+	file, err := conn.(*net.TCPConn).File()
+	fd := file.Fd()
+	err = wolfSSL.WolfSSL_set_fd(ssl, int(fd))
+	if err != nil {
+		fmt.Println("Error: WolfSSL_set_fd Failed")
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	/* Establish TLS connection */
+	ret = wolfSSL.WolfSSL_accept(ssl)
+	if ret != wolfSSL.WOLFSSL_SUCCESS {
+		ret = wolfSSL.WolfSSL_get_error(ssl, ret)
+		fmt.Println("Error: WolfSSL_accept Failed:", ret)
+		message := make([]byte, 256)
+		wolfSSL.WolfSSL_ERR_error_string(ret, message)
+		fmt.Println("Error:", string(message))
+		file.Close()
+		return
+	} else {
+		fmt.Println("Client Successfully Connected!")
+	}
+
+	buf := make([]byte, 1000000)
+
+	/* Receive then print the message from client */
+	ret = wolfSSL.WolfSSL_read(ssl, buf, 100000)
+	if ret == -1 {
+		fmt.Println("WolfSSL_read failed")
+	} else {
+		fmt.Println("Client says:", string(buf))
+	}
+
+	/* Create the message and send to client */
+	reply := []byte("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello World!\n")
+	sz := uintptr(len(reply))
+
+	ret = wolfSSL.WolfSSL_write(ssl, reply, sz)
+	if uintptr(ret) != sz {
+		fmt.Println("WolfSSL_write failed")
+		os.Exit(1)
+	}
+
+	/* Close the connection */
+	conn.Close()
+	wolfSSL.WolfSSL_shutdown(ssl)
+	wolfSSL.WolfSSL_free(ssl)
+}
